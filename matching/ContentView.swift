0fbb9092a3b3f56875236efd84aea5cbc04a7f93
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2023-09-17 10:53:23 -0400

    Initial Commit

---
+++ matching/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  matching
+//
+//  Created by J C on 9/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
