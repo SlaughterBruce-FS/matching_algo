bcf0e99be183a7804a370911980d591945e1fac6
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2023-09-17 10:53:23 -0400

    Initial Commit

---
+++ matching/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  matching
+//
+//  Created by J C on 9/17/23.
+//
+
+import Foundation
---
+++ matching/matchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchingApp.swift
+//  matching
+//
+//  Created by J C on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct matchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
